d58ba283771d13353ed98d43b9442ce729861e2b
Jfontaine <jfontaine19@gmail.com>
2022-05-18 10:30:13 -0400

    created an infobox that contains categorical data displayed on the page

---
+++ src/InfoBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./InfoBox.css";
+import { Card, CardContent, Typography } from "@mui/material";
+
+function InfoBox({ title, cases, total }) {
+  return (
+    <Card className="infobox">
+      <CardContent>
+        {/** Title */}
+        <Typography className="infobox__title" color="textSecondary">
+          {title}
+        </Typography>
+
+        {/** Number of cases */}
+        <h2 className="infobox__cases">{cases}</h2>
+
+        {/** Total */}
+        <Typography className="infobox__total" color="textSecondary">
+          {total}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
