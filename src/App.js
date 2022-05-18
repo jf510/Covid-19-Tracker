21ddb6941edae9a774ff0cb4d3539489347bb67e
Jfontaine <jfontaine19@gmail.com>
2022-05-17 22:44:18 -0400

    imported material ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { MenuItem, FormControl, Select } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select varient="outlined" value="abc">
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Option 4</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      {/** HEADER */}
+      {/** Title + Dropdown field */}
+      {/** InfoBoxs */}
+      {/** InfoBoxs */}
+      {/** InfoBoxs */}
+
+      {/** Table */}
+      {/** Graph */}
+
+      {/** Map */}
+    </div>
+  );
 }
 
 export default App;
