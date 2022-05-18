d040d06818ce950606b70e9c66da75d342facd83
Jfontaine <jfontaine19@gmail.com>
2022-05-18 14:26:23 -0400

    added function to sort table data

---
+++ src/util.js
@@ -0,0 +1,20 @@
+/*
+export const sortData = (data) => {
+  const sortedData = [...data];
+  sortedData.sort((a, b) => {
+    if (a.cases > b.cases) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return sortedData;
+};
+
+*/ //  MUCH NICER
+
+export const sortData = (data) => {
+  const sortedData = [...data];
+
+  return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+};
