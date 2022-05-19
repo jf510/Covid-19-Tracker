e85b9a7399d97710176fc13ad72552411a3c388c
Jfontaine <jfontaine19@gmail.com>
2022-05-19 09:31:29 -0400

    removed comments

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -6,15 +6,12 @@ function InfoBox({ title, cases, total }) {
   return (
     <Card className="infobox">
       <CardContent>
-        {/** Title */}
         <Typography className="infobox__title" color="textSecondary">
           {title}
         </Typography>
 
-        {/** Number of cases */}
         <h2 className="infobox__cases">{cases} today</h2>
 
-        {/** Total */}
         <Typography className="infobox__total" color="textSecondary">
           {total} total
         </Typography>
