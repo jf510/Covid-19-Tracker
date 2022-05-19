e5b40b4315567664af6e84a8b6a52a8bf8efb4bb
Jfontaine <jfontaine19@gmail.com>
2022-05-19 09:29:23 -0400

    created linegraph component and added options object. Set aspectratio to true. implemented useEffect to fetch data from disease.sh

---
+++ src/LineGraph.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import { Line, Chart } from "react-chartjs-2";
+import numeral from "numeral";
+import { Chart as ChartJS } from "chart.js/auto";
+
+
+const options = {
+  
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: true,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        suggestedMin: 50,
+
+        gridlines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
+
+const buildChartData = (data, casesType = "cases") => {
+  const chartData = [];
+  let lastDataPoint;
+
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: data["cases"][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+function LineGraph(casesType = "cases") {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+          const chartData = buildChartData(data, "cases");
+          setData(chartData);
+        });
+    };
+    fetchData();
+  }, [casesType]);
+
+  return (
+    <div className="lineGraph">
+      
+      {data?.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204, 16, 52, 0.6)",
+                borderColor: "#CC1034",
+                data: data,
+              },
+            ],
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default LineGraph;
